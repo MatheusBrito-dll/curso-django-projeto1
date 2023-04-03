782d3e508c508d9e9c3cf69a01ee7275a01eade5
Matheus Santos Brito <matheusbrito0253@gmail.com>
2023-04-03 17:48:10 -0400

    Removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from recipes.views import home, contato, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),
-    path('home', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
 ]
