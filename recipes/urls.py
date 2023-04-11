804f2bfcccf052046aa29a81e9f4f66372924cf7
Matheus Santos Brito <matheusbrito0253@gmail.com>
2023-04-11 17:17:53 -0400

    teste html

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import home
+from recipes.views import home, teste
 
 urlpatterns = [
     path('', home),
+    path('teste', teste),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,7 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Matheus Brito',
     })
+
+
+def teste(request):
+    return render(request, 'recipes/pages/teste.html')
